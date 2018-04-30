99cc6c8b436e197393c3d9f50a8761db59e8a4bd
alrus2797 <alrus2797@gmail.com>
2018-04-30 09:56:47 -0500

    Fix Frontend issues

--- resources/views/superUsuario/main.blade.php
+++ resources/views/superUsuario/main.blade.php
@@ -12,29 +12,29 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="HandheldFriendly" content="True">
     <meta name="MobileOptimized" content="320">
-    <link rel="icon" href="./favicon.ico" type="image/x-icon">
-    <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
+    <link rel="icon" href="{{url('/favicon.ico')}}" type="image/x-icon">
+    <link rel="shortcut icon" type="image/x-icon" href="{{url('/favicon.ico')}}">
     <!-- Generated: 2018-04-06 16:27:42 +0200 -->
     @yield('title')
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
-    <script src="/assets/js/require.min.js"></script>
+    <script src="{{url('/assets/js/require.min.js')}}"></script>
     <script>
       requirejs.config({
           baseUrl: '.'
       });
     </script>
     <!-- Dashboard Core -->
-    <link href="/assets/css/dashboard.css" rel="stylesheet">
-    <script src="/assets/js/dashboard.js"></script>
+    <link href="{{url('/assets/css/dashboard.css')}}" rel="stylesheet">
+    <script src="{{url('/assets/js/dashboard.js')}}"></script>
     <!-- c3.js Charts Plugin -->
-    <link href="/assets/plugins/charts-c3/plugin.css" rel="stylesheet">
-    <script src="/assets/plugins/charts-c3/plugin.js"></script>
+    <link href="{{url('/assets/plugins/charts-c3/plugin.css')}}" rel="stylesheet">
+    <script src="{{url('/assets/plugins/charts-c3/plugin.js')}}"></script>
     <!-- Google Maps Plugin -->
-    <link href="/assets/plugins/maps-google/plugin.css" rel="stylesheet">
-    <script src="/assets/plugins/maps-google/plugin.js"></script>
+    <link href="{{url('/assets/plugins/maps-google/plugin.css')}}" rel="stylesheet">
+    <script src="{{url('/assets/plugins/maps-google/plugin.js')}}"></script>
     <!-- Input Mask Plugin -->
-    <script src="/assets/plugins/input-mask/plugin.js"></script>
+    <script src="{{url('/assets/plugins/input-mask/plugin.js')}}"></script>
   </head>
   <body class="">
     <div class="page">
