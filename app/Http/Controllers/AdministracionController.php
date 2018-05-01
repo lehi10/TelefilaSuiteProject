d2a3c447822fea5bbdf3ec8ab01d9bb34877922b
alrus2797 <alrus2797@gmail.com>
2018-05-01 16:45:43 -0500

    fix

--- app/Http/Controllers/AdministracionController.php
+++ app/Http/Controllers/AdministracionController.php
@@ -12,7 +12,7 @@ class AdministracionController extends Controller
     
     public function __construct()
     {
-        $this->middleware('auth:administrador');
+        $this->middleware('auth');
     }
 
     public function index(Request $request, $idCliente)
