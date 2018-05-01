ee805a3ac0ba54c50fce3de55d6a7c65988a6be2
alrus2797 <alrus2797@gmail.com>
2018-05-01 12:27:55 -0500

    show name

--- resources/views/superUsuario/index.blade.php
+++ resources/views/superUsuario/index.blade.php
@@ -17,8 +17,7 @@
                     cliente</a> </div>
                 <div class="dropdown"> <a href="#" class="nav-link pr-0 leading-none"
                     data-toggle="dropdown"> <span class="avatar" style="background-image: url(./demo/faces/female/25.jpg)"></span>
-                    <span class="ml-2 d-none d-lg-block"> <span class="text-default">Rolando
-                        Ancajima</span> <small class="text-muted d-block mt-1">Administrator</small>
+                    <span class="ml-2 d-none d-lg-block"> <span class="text-default">{{Auth::user()->username}}</span> <small class="text-muted d-block mt-1">Administrator</small>
                     </span> </a>
                   <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
                     <a class="dropdown-item" href="#"> <i class="dropdown-icon fe fe-user"></i>
