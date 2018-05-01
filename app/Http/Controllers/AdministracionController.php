7ff1ab88b4fe865f3c82006bb06a2875ea290e7c
alrus2797 <alrus2797@gmail.com>
2018-05-01 18:02:18 -0500

    login admin

--- app/Http/Controllers/AdministracionController.php
+++ app/Http/Controllers/AdministracionController.php
@@ -12,7 +12,7 @@ class AdministracionController extends Controller
     
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     public function index(Request $request, $idCliente)
