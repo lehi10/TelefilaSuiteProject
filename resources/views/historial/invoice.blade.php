ab530b75dd35d1365a066b33ddcc19efe314ca23
Lehi Qr <lehi_10@hotmail.com>
2019-02-16 14:02:15 -0500

    Adding  styles to pdf generator

--- resources/views/historial/invoice.blade.php
+++ resources/views/historial/invoice.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="es">
-<head><title>Hospital {{$hospital->nombre}} {{$fecha}}| Reporte</title></head>
+<head><title>Hospital {{$hospital->nombre}} {{$fecha}}| Reporte</title>
+<link rel="stylesheet" href="pdfStyles/style.css" media="all" />
+</head>
   <body>
     <header class="clearfix">
       <div>
