4c29d95943bb5de9a57c599af2215364b919e709
Lehi Qr <lehi_10@hotmail.com>
2019-02-16 13:40:45 -0500

    Reportes  invoice pdf test

--- resources/views/historial/invoice.blade.php
+++ resources/views/historial/invoice.blade.php
@@ -1,10 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <title>Hospital {{$hospital->nombre}} {{$fecha}}  | Reporte</title>
-    <link rel="stylesheet" href="pdfStyles/style.css" media="all" />
-  </head>
   <body>
 Hello World
   </body>
