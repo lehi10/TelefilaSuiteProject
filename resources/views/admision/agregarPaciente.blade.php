49f6e48656c5fa086d330578e6245e23b089dce7
Lehi Qr <lehi_10@hotmail.com>
2019-03-29 23:16:50 -0500

    https ajax

--- resources/views/admision/agregarPaciente.blade.php
+++ resources/views/admision/agregarPaciente.blade.php
@@ -21,8 +21,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
     <script src="/assets/js/require.min.js"></script>
-    <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jquery/jquery-1.7.1.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jquery/jquery-1.7.1.min.js"></script>
 
 
     <script>
